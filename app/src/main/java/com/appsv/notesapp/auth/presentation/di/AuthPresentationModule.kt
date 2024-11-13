75a67b51a1b3b8f05d11c80272ee664920e35aaf
Viraj76 <virajg.7680@gmail.com>
2024-11-14 01:54:25 +0530

    providing dependencies via Koin

--- app/src/main/java/com/appsv/notesapp/auth/presentation/di/AuthPresentationModule.kt
+++ app/src/main/java/com/appsv/notesapp/auth/presentation/di/AuthPresentationModule.kt
@@ -1,11 +1,16 @@
 package com.appsv.notesapp.auth.presentation.di
 
 import android.content.Context
+import com.appsv.notesapp.auth.presentation.SignInViewModel
 import com.appsv.notesapp.core.presentation.sign_in.GoogleAuthenticator
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val authPresentationModule = module {
-    single {(ctx: Context)->
-        GoogleAuthenticator(ctx)
+    single {(context: Context)->
+        GoogleAuthenticator(context)
+    }
+    viewModel { (context:Context)->
+        SignInViewModel(context)
     }
 }
