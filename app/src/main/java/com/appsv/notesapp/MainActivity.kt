bb4b41ed4b006b963b6db7466d79d0682a27517a
Viraj76 <virajg.7680@gmail.com>
2024-11-13 21:22:07 +0530

    SignIn Fragment

--- app/src/main/java/com/appsv/notesapp/MainActivity.kt
+++ app/src/main/java/com/appsv/notesapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.appsv.notesapp
 
+import android.graphics.Color
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.ColorInt
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,12 +12,20 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            // changing status bar background color.
+            statusBarStyle = SystemBarStyle.dark(
+                0xFF3450a1.toInt()
+            )
+        )
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
---
+++ app/src/main/java/com/appsv/notesapp/auth/presentation/SignInFragment.kt
@@ -0,0 +1,24 @@
+package com.appsv.notesapp.auth.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.appsv.notesapp.R
+import com.appsv.notesapp.databinding.FragmentSignInBinding
+
+class SignInFragment : Fragment() {
+    private lateinit var binding : FragmentSignInBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignInBinding.inflate(layoutInflater)
+
+
+        return binding.root
+    }
+
+
+}
